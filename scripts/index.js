817d24e9dedfeea259715845cfb6af5c92967faf
andreybozhkov <bozhkov.andrey@gmail.com>
2020-02-02 22:05:54 +0200

    Added initial trailer component

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,17 @@
 function startGame() {
     gameArea.start();
+    trailer = new component(40, 20, 'blue', 10, 10);
 }
+
+function component (width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    context = gameArea.context;
+    context.fillStyle = color;
+    context.fillRect(this.x, this.y, this.width, this.height);
+};
 
 let gameArea = {
     canvas: (() => {
