8f618ddacb3a2efaf6825014cc3a70882a7eccda
andreybozhkov <bozhkov.andrey@gmail.com>
2020-02-02 23:13:29 +0200

    Added mouse event listener prototype 1

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,16 @@ let gameArea = {
         let gameContainer = document.getElementById('gameContainer');
         gameContainer.appendChild(this.canvas);
         this.interval = setInterval(updateGameArea, 20);
+        gameContainer.addEventListener('mousedown', (e) => {
+            gameArea.x = e.pageX;
+            gameArea.y = e.pageY;
+            console.log(gameArea.x);
+            console.log(gameArea.y);
+        });
+        gameContainer.addEventListener('mouseup', (e) => {
+            gameArea.x = false;
+            gameArea.y = false;
+        });
     },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -19,7 +29,7 @@ let gameArea = {
 
 function startGame() {
     gameArea.start();
-    trailer = new component(40, 20, 'blue', 10, 10);
+    trailer = new component(40, 20, 'blue', 20, 20);
 };
 
 function component (width, height, color, x, y) {
@@ -32,9 +42,25 @@ function component (width, height, color, x, y) {
         context.fillStyle = color;
         context.fillRect(this.x, this.y, this.width, this.height);
     };
+    this.clicked = () => {
+        let currentLeft = this.x;
+        let currentRight = this.x + this.width;
+        let currentTop = this.y;
+        let currentBottom = this.y + this.height;
+        let clicked = false;
+        if ((currentBottom >= gameArea.y) || (currentTop <= gameArea.y) || (currentLeft >= gameArea.x) || (currentRight <= gameArea.x)) {
+            clicked = false;
+        }
+        return clicked;
+    }
 };
 
 function updateGameArea() {
     gameArea.clear();
+    if (gameArea.x && gameArea.y) {
+        if (trailer.clicked()) {
+            trailer.x += 10;
+        }
+    }
     trailer.update();
 }
