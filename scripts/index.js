6e6409ce672400811cb5f14ce98e31d33660af5f
andreybozhkov <bozhkov.andrey@gmail.com>
2020-02-02 22:24:12 +0200

    Added canvas refresh

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,3 @@
-function startGame() {
-    gameArea.start();
-    trailer = new component(40, 20, 'blue', 10, 10);
-}
-
-function component (width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    context = gameArea.context;
-    context.fillStyle = color;
-    context.fillRect(this.x, this.y, this.width, this.height);
-};
-
 let gameArea = {
     canvas: (() => {
         let canvas = document.createElement('canvas');
@@ -25,5 +10,31 @@ let gameArea = {
         this.context = this.canvas.getContext("2d");
         let gameContainer = document.getElementById('gameContainer');
         gameContainer.appendChild(this.canvas);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
+};
+
+function startGame() {
+    gameArea.start();
+    trailer = new component(40, 20, 'blue', 10, 10);
+};
+
+function component (width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.update = () => {
+        context = gameArea.context;
+        context.fillStyle = color;
+        context.fillRect(this.x, this.y, this.width, this.height);
+    };
+};
+
+function updateGameArea() {
+    gameArea.clear();
+    trailer.update();
 }
